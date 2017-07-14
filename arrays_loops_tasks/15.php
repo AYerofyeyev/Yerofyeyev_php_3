f76944c250f65bf600281d98b932efa7a225761a
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-14 14:19:24 +0300

    Task 15 is done

---
+++ arrays_loops_tasks/15.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 14.07.2017
+ * Time: 13:29
+ */
+
+$arr = [4, 2, 5, 19, 13, 0, 10];
+$count = 0;
+
+foreach ($arr as $value) {
+    $count++;
+}
+
+echo $count;
