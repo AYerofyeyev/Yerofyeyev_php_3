d3c76f33720876b64ce3a0d736909965e879a092
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-14 13:09:52 +0300

    Tasks 10 and 11 are done

---
+++ arrays_loops_tasks/10.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 14.07.2017
+ * Time: 12:56
+ */
+
+$a = 11;
+while ($a >= 11 && $a <= 33) {
+    echo $a . "<br/>";
+    $a++;
+}
---
+++ arrays_loops_tasks/11.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 14.07.2017
+ * Time: 13:06
+ */
+
+for ($i = 0; $i <= 100; $i +=2) {
+    echo $i . "<br/>";
+}
