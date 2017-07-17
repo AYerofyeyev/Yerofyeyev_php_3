5954b5a1ea01243c094c25d8a4eceed872c52ead
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-17 11:03:50 +0300

    Task 16 is done

--- arrays_loops_tasks/16.php
+++ arrays_loops_tasks/16.php
@@ -6,3 +6,13 @@
  * Time: 17:16
  */
 
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+foreach ($arr as $key => $value) {
+    echo $value;
+    if ((++$key % 3) == 0) {
+        echo "<br />";
+  } else {
+        echo ", ";
+    }
+}
