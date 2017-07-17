f359e923c4cdd63cf67abeadd836864e67aa6ced
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-17 16:36:53 +0300

    Task 22 is done

---
+++ arrays_loops_tasks/22.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 17.07.2017
+ * Time: 16:35
+ */
+
+for ($i = 1; $i <=5; $i++) {
+    for ($j = 0; $j < ($i*2); $j++) {
+        echo "x";
+    }
+    echo "<br/>";
+}
