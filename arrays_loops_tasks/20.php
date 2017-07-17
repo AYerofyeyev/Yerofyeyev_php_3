96f3b1b299aad91128bb16e96d245db01ffc9586
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-17 16:32:06 +0300

    Task 20 is done

---
+++ arrays_loops_tasks/20.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 17.07.2017
+ * Time: 15:57
+ */
+
+for ($i = 1; $i <=20; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        echo "x";
+    }
+    echo "<br/>";
+}
