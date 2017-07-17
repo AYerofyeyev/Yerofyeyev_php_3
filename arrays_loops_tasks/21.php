df03638105da29d5acf0ae56774eff46da53d128
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-17 16:35:01 +0300

    Task 21 is done

---
+++ arrays_loops_tasks/21.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 17.07.2017
+ * Time: 16:32
+ */
+
+for ($i = 1; $i <=9; $i++) {
+    for ($j = 0; $j < $i; $j++) {
+        echo $i;
+    }
+    echo "<br/>";
+}
