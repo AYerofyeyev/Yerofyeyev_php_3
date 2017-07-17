07d69a8f8edbe871091a047af0abfa4aae5697d7
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-17 11:41:02 +0300

    Task 18 is done

---
+++ arrays_loops_tasks/18.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 17.07.2017
+ * Time: 11:23
+ */
+
+$arr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+foreach ($arr as $key => $value) {
+    switch ($key) {
+        case 5:
+        case 6:
+            echo "<strong>{$value}<strong>, ";
+            break;
+        default:
+            echo $value . ", ";
+    }
+}
