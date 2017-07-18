6d2a1bc51a0a7aad5128e630929499bc405f94c0
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-18 17:45:26 +0300

    Task 2 and 13 are started

---
+++ functions_forms_tasks/2.php
@@ -0,0 +1,21 @@
+<form method="post">
+    <textarea name = "myText"></textarea>
+</form>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ean
+ * Date: 18.07.2017
+ * Time: 11:56
+ */
+
+function top3($a){
+    $arr = explode(' ', $a);
+
+}
+
+$myText = $_POST['myText'];
+
+top3($myText);
+
+?>
