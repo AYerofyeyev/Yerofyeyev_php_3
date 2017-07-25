91a42c75ef0b41bd75c2441be4d7cf64b4f365da
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-07-25 20:05:05 +0300

    Task 2 is done

--- functions_forms_tasks/2.php
+++ functions_forms_tasks/2.php
@@ -1,5 +1,6 @@
 <form method="post">
     <textarea name = "myText"></textarea>
+    <input type="submit">
 </form>
 <?php
 /**
@@ -10,12 +11,18 @@
  */
 
 function top3($a){
+    $arrCounts = $keys =[];
     $arr = explode(' ', $a);
-
+    foreach ($arr as $value) {
+        array_push($arrCounts, strlen($value));
+    }
+        arsort($arrCounts);
+        $keys = array_keys($arrCounts);
+    echo $arr[$keys[0]] . "<br />";
+    echo $arr[$keys[1]] . "<br />";
+    echo $arr[$keys[2]] . "<br />";
 }
 
 $myText = $_POST['myText'];
 
 top3($myText);
-
-?>
